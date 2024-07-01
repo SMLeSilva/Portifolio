496824789bdbed3ddf8bc78201c924024a8bbee9
Samuel Rodrigues da Silva <samuel.silva.rodrigues04@gmail.com>
2024-07-01 15:59:26 -0300

    Mudança de estilo da pagina para preto e amarelo, troca de imagem e informações do header

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
-/* --- NavBar --- */
+// NAVBAR 
+
 window.addEventListener("scroll", function () {
     let navbar__width = document.querySelector('#navbar')
-    navbar__width.classList.toggle('width100', window.scrollY > 100)
+    navbar__width.timeout = setTimeout(function () {
+        navbar__width.classList.toggle('width100', window.scrollY > 10);
+    }, 2000);
 })
 
-/* --- Slider About --- */
+// SLIDER ABOUT 
 
 const button__about = document.getElementById('button__about__me')
 const slider = document.querySelector('.slider__about__me')
@@ -19,8 +22,8 @@ function controlSlide() {
         sliderNumber = 1;
         about2.style.top = '0'
         about2.style.left = '0'
-        about2.style.boxShadow = '-15px 0 15px -5px rgba(255, 255, 255, 0.5)'
-        slider.style.boxShadow = '0 0 15px 8px rgba(255, 255, 255, 0.5)'
+        about2.style.boxShadow = '-15px 0 15px -5px rgba(255, 255, 255, 0.244)'
+        slider.style.boxShadow = '0 0 15px 8px rgba(255, 255, 255, 0.244)'
         button__about.style.transform = 'translate(-50%, -50%) rotate(-180deg)';
     }
     else {
@@ -28,7 +31,21 @@ function controlSlide() {
         about2.style.top = '0'
         about2.style.left = '100%'
         about2.style.boxShadow = 'none'
-        slider.style.boxShadow = 'none'
+        slider.style.boxShadow = '-10px 15px 25px 0px rgba(48, 48, 48, 0.5)'
         button__about.style.transform = 'translate(-50%, -50%) rotate(0deg)';
+    }
+}
+
+// DARK LIGHT
+
+const check__box__dark__light = document.getElementById('checkbox__moon__switch')
+
+check__box__dark__light.addEventListener('change', switch__moon())
+
+function switch__moon(){
+    if(check__box__dark__light.checked){
+       
+    }else{
+
     }
 }
